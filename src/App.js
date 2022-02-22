c317079be779901f756b4a32fe23743aec5138fe
João 🦄 <joao@joao.codes>
2022-02-22 18:45:35 +0000

    feat: lay out initial index layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app">
+      {/* <Map /> */}
+      <div className="cards-wrapper">
+        <Card />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Card/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Card = () => (
+  <div className="card">
+    <p>TEST</p>
+  </div>
+)
+
+export default Card
