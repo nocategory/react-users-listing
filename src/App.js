543ef21ad25a675bbdc8572b9701e367de6dccd9
João 🦄 <joao@joao.codes>
2022-02-23 14:53:22 +0000

    feat: add api fetch & error handling logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 import Card from './components/Card'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+  const [status, setStatus] = useState('loading')
+
+  useEffect(() => {
+    async function getDataFromApi() {
+      await fetch('https://jsonplaceholder.typicode.com/usersfsdfdssfd')
+        .then((res) => {
+          if (res.ok) {
+            return res.json()
+          } else {
+            throw new Error('Something went wrong')
+          }
+        })
+        .then((data) => setData(data) && setStatus('loaded'))
+        .catch(() => setStatus('error'))
+    }
+    getDataFromApi()
+  }, [])
+
   return (
     <div className="app">
       {/* <Map /> */}
       <div className="cards-wrapper">
-        <Card />
+        {(() => {
+          switch (status) {
+            case 'loading':
+              return <p>Loading data *beep boop*</p>
+            case 'loaded':
+              return data.map((user) => <Card key={user.id} user={user} />)
+            default:
+              return <p>Something went wrong</p>
+          }
+        })()}
       </div>
     </div>
   )
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,15 @@
+import { useEffect } from 'react'
 import './index.css'
 
-const Card = () => (
-  <div className="card">
-    <p>TEST</p>
-  </div>
-)
+function Card({ user }) {
+  useEffect(() => {
+    console.log(user)
+  }, [user])
+  return (
+    <div className="card">
+      <p>TEST</p>
+    </div>
+  )
+}
 
 export default Card
