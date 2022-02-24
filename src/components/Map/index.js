2e203c6516b6d48daf8fbe6373233082eb0187de
João 🦄 <joao@joao.codes>
2022-02-24 21:33:11 +0000

    style: change map styling

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -16,10 +16,10 @@ function Map({ users }) {
       }}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        subdomains={['a', 'b', 'c', 'd']}
       />
-      {console.log(users)}
       {users.map((user) => (
         <Marker
           key={user.username}
@@ -27,7 +27,7 @@ function Map({ users }) {
           icon={L.divIcon({
             iconSize: [32, 32],
             className: 'mymarker',
-            html: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="#000" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>',
+            html: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="#fff" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>',
           })}
         >
           <Popup>
