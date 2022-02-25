10bdf54b2cb4b7b430bc8f0cd77e43185d27480f
João 🦄 <joao@joao.codes>
2022-02-25 11:26:43 +0000

    get all user data displayed

--- src/User.js
+++ src/User.js
@@ -1,6 +1,7 @@
 import Map from './components/Map'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import './User.css'
 
 function User() {
   const [data, setData] = useState([])
@@ -17,15 +18,65 @@ function User() {
             throw new Error('Something went wrong')
           }
         })
-        .then((data) => {
-          setData([data]) // data is an array of one user, since Map is using .map()
+        .then((apiData) => {
+          setData([apiData]) // data is an array of one user, since Map is using .map()
           setStatus('loaded')
         })
         .catch(() => setStatus('error'))
     }
     getDataFromApi()
   }, [id])
-  return <div>{status === 'loaded' && <Map users={data} />}</div>
+  return (
+    <div>
+      {status === 'loaded' && <Map users={data} />}
+      <div className="cards-wrapper">
+        {(() => {
+          switch (status) {
+            case 'loading':
+              return <p>Loading data *beep boop*</p>
+            case 'loaded':
+              return data.map((user) => (
+                <ul key={user.id}>
+                  <li>
+                    <b>Name: </b>
+                    {user.name}
+                  </li>
+                  <li>
+                    <b>Username: </b>
+                    {user.username}
+                  </li>
+                  <li>
+                    <b>Email: </b>
+                    {user.email}
+                  </li>
+                  <li>
+                    <b>Address: </b>
+                    {user.address.city} {user.address.street}{' '}
+                    {user.address.suite} {user.address.zipcode} |{' '}
+                    {user.address.geo.lat} {user.address.geo.lng}
+                  </li>
+                  <li>
+                    <b>Phone: </b>
+                    {user.phone}
+                  </li>
+                  <li>
+                    <b>Website: </b>
+                    {user.website}
+                  </li>
+                  <li>
+                    <b>Company: </b>
+                    <b>{user.company.name}</b> "{user.company.catchPhrase}"{' '}
+                    <b>bs:</b> {user.company.bs}
+                  </li>
+                </ul>
+              ))
+            default:
+              return <p>Something went wrong</p>
+          }
+        })()}
+      </div>
+    </div>
+  )
 }
 
 export default User
